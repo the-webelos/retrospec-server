8552339c1f63e52b3d514d2994bff997039f8141
jlorusso <jlorusso@carbonblack.com>
2018-05-20 01:06:36 -0400

    Get rid of unnecessary is_room_empty checks on unsubscribe and disconnect

--- backend/retro/websocket_server/app.py
+++ backend/retro/websocket_server/app.py
@@ -47,6 +47,31 @@ def error_handler(ex):
     _logger.error(ex)
 
 
+@socketio.on('connect', namespace=namespace)
+def test_connect():
+    global thread
+
+    with thread_lock:
+        if thread is None:
+            _logger.debug("Starting update listener thread...")
+            thread = socketio.start_background_task(board_engine.update_listener, message_cb)
+
+    emit('connect_response', {'data': 'Connected'})
+    _logger.debug("Connection successful for sid '%s'!", request.sid)
+
+
+@socketio.on('disconnect', namespace=namespace)
+def test_disconnect():
+    _logger.debug("Client '%s' successfully disconnected", request.sid)
+
+
+@socketio.on('disconnect_request', namespace=namespace)
+def disconnect_request():
+    _logger.debug("Disconnect request received from client '%s'.", request.sid)
+    emit('disconnect_response', {'data': 'Disconnected!'})
+    disconnect()
+
+
 @socketio.on('subscribe', namespace=namespace)
 def subscribe(message):
     board_id = message.get("board_id")
@@ -69,27 +94,7 @@ def unsubscribe(message):
     board_id = message['board_id']
     _logger.debug("Unsubscribing from board '%s'...", board_id)
     leave_room(board_id)
-
-    # If room is empty, close it down
-    if is_room_empty(board_id):
-        _logger.debug("Room '%s' is empty after unsubscribe", board_id)
-        close(board_id)
-
     emit('unsubscribe_response', {'board_id': board_id})
-
-
-def close(board_id):
-    _logger.debug("Closing room %s", board_id)
-
-    close_room(board_id)
-
-
-@socketio.on('disconnect_request', namespace=namespace)
-def disconnect_request():
-    _logger.debug("Disconnect request received from client.")
-    emit('disconnect_response', {'data': 'Disconnected!'})
-    disconnect()
-    _logger.debug("Disconnect successful")
 
 
 @socketio.on('my_ping', namespace=namespace)
@@ -97,40 +102,12 @@ def ping_pong():
     emit('my_pong')
 
 
-@socketio.on('connect', namespace=namespace)
-def test_connect():
-    global thread
-
-    with thread_lock:
-        if thread is None:
-            _logger.debug("Starting update listener thread...")
-            thread = socketio.start_background_task(board_engine.update_listener, message_cb)
-
-    emit('connect_response', {'data': 'Connected'})
-    _logger.debug("Connection successful for sid '%s'!", request.sid)
-
-
-@socketio.on('disconnect', namespace=namespace)
-def test_disconnect():
-    # If sid is the last client in any rooms, terminate the subscription and close the room. socketio will take care of
-    # the room close for us, but we need to stop the redis listener ourselves.
-    sid = request.sid
-    sid_rooms = socketio.server.manager.get_rooms(sid, namespace=namespace)
-    _logger.debug("Disconnecting sid '%s'. ROOMS=[%s]", sid, sid_rooms)
-    for room in sid_rooms:
-        if is_room_empty(room, ignore_sids=[sid]):
-            _logger.debug("Room '%s' is empty after '%s' disconnect", sid, room)
-            close(room)
-
-    _logger.debug('Client disconnected %s\n  ROOMS=%s', sid, sid_rooms)
-
-
 def message_cb(message, board_id):
     should_keep_listening = True
     event = json.loads(message['data'])
     event_type = event.get('event_type')
 
-    _logger.debug("Processing '%s' event", event_type)
+    _logger.debug("Processing '%s' event for board '%s'.", event_type, board_id)
 
     if event_type in ('node_update', 'node_del'):
         socketio.emit(event_type, {"nodes": event['event_data']}, namespace=namespace, room=board_id)
@@ -143,20 +120,6 @@ def message_cb(message, board_id):
         _logger.warning("Unknown event type for %s: %s", board_id, event_type)
 
     return should_keep_listening
-
-
-def is_room_empty(room, ignore_sids=()):
-    try:
-        participants = [p for p in socketio.server.manager.get_participants(namespace, room) if p not in ignore_sids]
-        _logger.debug("The following sids are still in room '%s': %s. NOTE: The following sids were ignored: %s",
-                      room, participants, ignore_sids)
-        # Check if anyone is still in the room
-        room_empty = bool(participants)
-    except KeyError:
-        # This will raise a KeyError if there are no participants in the room
-        room_empty = True
-
-    return room_empty
 
 
 if __name__ == "__main__":
