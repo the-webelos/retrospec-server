7229165cecca291a13f2b560c94076fd459c7406
jlorusso <jlorusso@carbonblack.com>
2018-06-01 01:40:15 -0400

    Improve flexibility of logging setup

--- backend/retro/utils/retro_logging.py
+++ backend/retro/utils/retro_logging.py
@@ -4,4 +4,9 @@ import logging
 def setup_basic_logging(filename=None, level=logging.INFO, fmt=None):
     if not fmt:
         fmt = "%(asctime)s [%(levelname)s] %(name)s - %(message)s"
-    logging.basicConfig(filename=filename, level=level, format=fmt)
+
+    handlers = [logging.StreamHandler()]
+    if filename:
+        handlers.append(logging.FileHandler(filename))
+
+    logging.basicConfig(level=level, format=fmt, handlers=handlers)
