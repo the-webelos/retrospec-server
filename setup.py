e8bb8afa7a5605c3ddbd1682c242259ecfeca32c
Jacob Hackett <jacobrhackett@gmail.com>
2018-06-18 19:03:43 -0400

    moving everything out of the backend folder

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 install_requires = ['redis>=2.10.6', 'flask-socketio>=3.0.0', 'eventlet>=0.23.0']
-testing_requires = ['boto3==1.7.19', 'docker>=3.1.0', 'redis>=2.10.6']
+testing_requires = ['boto3==1.7.19', 'docker>=3.1.0', 'redis>=2.10.6', 'pytest']
 
 setup(
     name='retrospec-server',
