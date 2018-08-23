899c08db7bff6828243f1625b6caa15f1a76e562
Nicholas Murdock <nmurdock@carbonblack.com>
2018-08-23 16:24:44 -0500

    Add route to import cards from image
    Using opencv (with updated docker image) and aws rekognotion
    to inspect an uploaded image and turn it into cards on the
    requested board.

---
+++ backend/retro/engine/image_engine.py
@@ -0,0 +1,20 @@
+import logging
+
+from retro.image.card_reader import CardReader
+
+_logger = logging.getLogger(__name__)
+
+
+class ImageEngine(object):
+    def __init__(self, board_engine):
+        self.board_engine = board_engine
+
+    def import_cards(self, board_id: str, parent_id: str, image_bytes: str):
+        card_reader = CardReader(image_bytes)
+        cards = card_reader.get_cards_content()
+
+        nodes = []
+        for card in cards:
+            nodes.append(self.board_engine.add_node(board_id, parent_id, card))
+
+        return nodes
